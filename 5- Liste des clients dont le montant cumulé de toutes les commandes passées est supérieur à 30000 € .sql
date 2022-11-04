f66c22d76262d4b5523cf9d2491c47846d963fe8
Yazou777 <wilga.kevin@sfr.fr>
2022-11-04 10:26:53 +0100

    ajout de commentaire

--- 5- Liste des clients dont le montant cumulé de toutes les commandes passées est supérieur à 30000 € .sql
+++ 5- Liste des clients dont le montant cumulé de toutes les commandes passées est supérieur à 30000 € .sql
@@ -3,5 +3,5 @@ FROM customers
 JOIN orders ON orders.CustomerID = customers.CustomerID
 JOIN `order details` ON `order details`.OrderID = orders.OrderID
 GROUP BY CompanyName
-HAVING SUM(UnitPrice*Quantity) > 30000
-ORDER BY CA desc
+HAVING SUM(UnitPrice*Quantity) > 30000    /*pour mettre une condition sur une agrégation(calcul) il faut utiliser Having */
+ORDER BY CA desc                          /*la clause HAVING est TOUJOURS utilisée en conjonction avec la clause GROUP BY /
